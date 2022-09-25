b4429372be00437b09053cccd03c3700c00a71eb
hirosphere-ja <51845520+hirosphere-ja@users.noreply.github.com>
2022-09-25 22:37:13 +0900

    ファイルを整理
    Appファイルからヘッダー部分を分割してコンポーネント化
    各コンポーネントをcomponentsディレクトリを作成して整理
    不要なassetディレクトリをファイルごと削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import APITest1 from "./APITest1";
+import APITest1 from "./components/APITest1";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="w-4/5 container mx-auto">
-      <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around">
-        <h1 className="text-3xl font-bold underline">APITest</h1>
-        <p className="m-0 text-gray-400">APITestです</p>
-      </div>
-      <div>
-        <APITest1 />
-      </div>
+      <Header />
+      <APITest1 />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around">
+      <h1 className="text-3xl font-bold underline">APITest</h1>
+      <p className="m-0 text-gray-400">APITestです</p>
+    </div>
+  );
+}
+
+export default Header;
